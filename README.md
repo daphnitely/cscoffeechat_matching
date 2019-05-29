1d7dd51949cf7f9ba19ded6131c46a53c0294823
Sherry Yuan <frostyyshadows@gmail.com>
2019-05-29 06:43:36 -0700

    getting a 202 but didn't receive email

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # cscoffeechat_matching
+
+# Setting up environment variables
+https://github.com/sendgrid/sendgrid-python#setup-environment-variables
--- send_email.py
+++ send_email.py
@@ -3,8 +3,9 @@ import os
 from sendgrid.helpers.mail import *
 
 sg = sendgrid.SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-from_email = "test@example.com"
-to_email = "test@example.com"
+# TODO: replace with the official cs coffee chat email and update API key
+from_email = "example@gmail.com"
+to_email = "example@gmail.com"
 subject = "Sending with SendGrid is Fun"
 content = "<strong>and easy to do anywhere, even with Python</strong>"
 mail = Mail(from_email, to_email, subject, content)
